816d1b583d6dd1fbc8450276a60f57fc72b537b2
andre-marques-a22207598 <127451848+andre-marques-a22207598@users.noreply.github.com>
2025-12-16 00:15:17 +0000

    final version after review

--- components/Produtos/Produtos.tsx
+++ components/Produtos/Produtos.tsx
@@ -6,7 +6,6 @@ import { Produto } from "@/models/interfaces"
 import Link from "next/link"
 import Pesquisa from "./Pesquisa"
 import Card from "./Card"
-import Navegar from "@/components/Produtos/Navegar"
 
 const url_api = "https://deisishop.pythonanywhere.com"
 const url_api_produtos = `${url_api}/products/`
@@ -52,7 +51,7 @@ export default function Produtos() {
                 <Pesquisa produtos={produtos} onFiltrar={setProdutosFiltrados} />
             </section>
 
-            <section className="absolute top-[-70px] right-[10px]">
+            <section className="absolute top-[-70px]">
                 <Link href="/produtos/carrinho" className="flex flex-col items-center w-fit">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
                         <path d="M221-120q-27 0-48-16.5T144-179L42-549q-5-19 6.5-35T80-600h190l176-262q5-8 14-13t19-5q10 0 19 5t14 13l176 262h192q20 0 31.5 16t6.5 35L816-179q-8 26-29 42.5T739-120H221Zm-1-80h520l88-320H132l88 320Zm260-80q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM367-600h225L479-768 367-600Zm113 240Z"/>
@@ -75,10 +74,6 @@ export default function Produtos() {
                         onAlterarCarrinho={setCarrinho}
                     />
                 ))}
-            </section>
-
-            <section>
-                <Navegar produtos={produtos} onFiltrar={setProdutosFiltrados} />
             </section>
         </div>
     )
